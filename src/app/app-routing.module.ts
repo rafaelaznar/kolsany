c8eab7619194b71db73fa0202829f045b2e96235
Rafael Aznar <rafa.aznar@gmail.com>
2021-10-01 19:03:17 +0200

    commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LogoutComponent } from './logout/logout.component';
+import { SecretComponent } from './secret/secret.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent},  
+  { path: 'login', component: LoginComponent },  
+  { path: 'logout', component: LogoutComponent },  
+  { path: 'secret', component: SecretComponent },
+  { path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

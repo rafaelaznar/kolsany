ffd2f84bf508e58a2b8288f94f7a9751c54aee64
RAFAEL AZNAR <rafaaznar@gmail.com>
2021-10-11 09:25:00 +0200

    limpieza cryptoservice

--- src/app/service/crypto.service.ts
+++ src/app/service/crypto.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
-//import * as CryptoJS from 'crypto-js';
-
 declare let CryptoJS: any;
-
 @Injectable({
   providedIn: 'root'
 })
@@ -10,37 +7,8 @@ declare let CryptoJS: any;
 export class CryptoService {
   constructor() { }
 
-  
   getSHA256(value: string) {
     return CryptoJS.SHA256(value).toString(CryptoJS.enc.Hex);
   }
 
-  //The set method is use for encrypt the value.
-  set(keys: string, value: string) {
-    var key = CryptoJS.enc.Utf8.parse(keys);
-    var iv = CryptoJS.enc.Utf8.parse(keys);
-    var encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(value.toString()), key,
-      {
-        keySize: 128 / 8,
-        iv: iv,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7
-      });
-
-    return encrypted.toString();
-  }
-
-  //The get method is use for decrypt the value.
-  get(keys: string, value: string) {
-    var key = CryptoJS.enc.Utf8.parse(keys);
-    var iv = CryptoJS.enc.Utf8.parse(keys);
-    var decrypted = CryptoJS.AES.decrypt(value, key, {
-      keySize: 128 / 8,
-      iv: iv,
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7
-    });
-
-    return decrypted.toString(CryptoJS.enc.Utf8);
-  }
 }
